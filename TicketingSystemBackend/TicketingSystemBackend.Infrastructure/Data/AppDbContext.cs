1758f24d7e8aa8c3d10ff85dab9b55237be87074
EXakiToR <83112883+EXakiToR@users.noreply.github.com>
2025-08-04 10:45:49 +0300

    add dbcontext, repository in Infrastructure (#10)
    * Add AppDbContext, CRUD in TicketRepository
    
    * add GetByTitleAsync
    
    * add CRUD ArticleRepository

---
+++ TicketingSystemBackend/TicketingSystemBackend.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketingSystemBackend.Domain.Entities;
+using File = TicketingSystemBackend.Domain.Entities.File;
+
+namespace TicketingSystemBackend.Infrastructure.Data;
+public class AppDbContext : DbContext
+{
+    public DbSet<Article> Articles { get; set; }
+    public DbSet<ArticleCategory> ArticleCategories { get; set; }
+    public DbSet<File> Files { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<TicketCategory> TicketCategories { get; set; }
+    public DbSet<TicketComment> TicketComments { get; set; }
+
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected AppDbContext()
+    {
+    }
+}

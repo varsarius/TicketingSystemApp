dfd05fcecefb22f23bf4f0bbbff027b2b2ab3222
Varsarius <mihailpancenco@gmail.com>
2025-08-07 09:53:49 +0300

    commit before merge from main.

--- TicketingSystemFrontend/TicketingSystemFrontend/TicketingSystemFrontend.Client/DTOs/ArticleDto.cs
+++ TicketingSystemFrontend/TicketingSystemFrontend/TicketingSystemFrontend.Client/DTOs/ArticleDto.cs
@@ -2,29 +2,18 @@
 
 public class ArticleDto
 {
-    //public int Id { get; set; }
-
-    //public int ArticleCategoryId { get; set; }
-    //public string ArticleCategoryName { get; set; } = null!;
-
-    //public string UserName { get; set; } = null!;
-
-    //public string Title { get; set; } = null!;
-    //public string Description { get; set; } = null!;
-
-    //public DateTime CreatedAt { get; set; }
-    //public DateTime UpdatedAt { get; set; }
-
     public int Id { get; set; }
 
     public int ArticleCategoryId { get; set; }
+    public string ArticleCategoryName { get; set; } = null!;
 
-    public int UserId { get; set; }
+    public string UserName { get; set; } = null!;
 
     public string Title { get; set; } = null!;
     public string Description { get; set; } = null!;
 
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
+
 
 }

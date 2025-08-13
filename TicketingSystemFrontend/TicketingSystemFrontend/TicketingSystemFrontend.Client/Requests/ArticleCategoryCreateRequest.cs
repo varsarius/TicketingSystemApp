7e5fd868d42800027173cb6b62847a3121ea26c6
Andrey Uzun <uzun.andrei13@gmail.com>
2025-08-13 16:42:40 +0300

    added create option for editing article

---
+++ TicketingSystemFrontend/TicketingSystemFrontend/TicketingSystemFrontend.Client/Requests/ArticleCategoryCreateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TicketingSystemFrontend.Client.Requests
+{
+    public class ArticleCategoryCreateRequest
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}

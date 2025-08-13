826c3cc80df683d2b8ee2dacb91dfea80bb70a3f
Andrey Uzun <uzun.andrei13@gmail.com>
2025-08-13 14:09:00 +0300

    dannye

--- TicketingSystemBackend/TicketingSystemBackend.Application/QueryHandlers/Articles/GetArticleByIdQueryHandler.cs
+++ TicketingSystemBackend/TicketingSystemBackend.Application/QueryHandlers/Articles/GetArticleByIdQueryHandler.cs
@@ -1,19 +1,20 @@
 ﻿using MediatR;
+using TicketingSystemBackend.Application.DTOs;
 using TicketingSystemBackend.Application.Interfaces;
 using TicketingSystemBackend.Application.Queries.Articles;
 using TicketingSystemBackend.Domain.Entities;
 
 namespace TicketingSystemBackend.Application.QueryHandlers.Articles;
-public class GetArticleByIdQueryHandler : IRequestHandler<GetArticleByIdQuery, Article>
+public class GetArticleByIdQueryHandler : IRequestHandler<GetArticleByIdQuery, ArticleDto>
 {
-    private readonly IArticleRepository _repository;
+    private readonly IArticleReadRepository _repository;
 
-    public GetArticleByIdQueryHandler(IArticleRepository repository)
+    public GetArticleByIdQueryHandler(IArticleReadRepository repository)
     {
         _repository = repository;
     }
 
-    public async Task<Article> Handle(GetArticleByIdQuery request, CancellationToken cancellationToken)
+    public async Task<ArticleDto> Handle(GetArticleByIdQuery request, CancellationToken cancellationToken)
     {
         return await _repository.GetByIdAsync(request.Id);
     }

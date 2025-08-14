eb3b1ff9040b8ab452ee1ab21003aa112092d2cf
Andrei Elciscev <andrey.elciscev@gmail.com>
2025-08-14 17:09:19 +0300

    add await to some repositories to fix DeleteByIdAsync, add Async postfix to controller methods

--- TicketingSystemBackend/TicketingSystemBackend.Api/Controllers/IController.cs
+++ TicketingSystemBackend/TicketingSystemBackend.Api/Controllers/IController.cs
@@ -4,9 +4,9 @@ namespace TicketingSystemBackend.Api.Controllers;
 public interface IController<TCreateCommand, TUpdateCommand>
 {
 
-    Task<IActionResult> Create([FromBody] TCreateCommand command);
-    Task<IActionResult> GetById(int id);
-    Task<IActionResult> GetAll();
-    Task<IActionResult> Update(int id, [FromBody] TUpdateCommand command);
-    Task<IActionResult> DeleteById(int id);
+    Task<IActionResult> CreateAsync([FromBody] TCreateCommand command);
+    Task<IActionResult> GetByIdAsync(int id);
+    Task<IActionResult> GetAllAsync();
+    Task<IActionResult> UpdateAsync(int id, [FromBody] TUpdateCommand command);
+    Task<IActionResult> DeleteByIdAsync(int id);
 }

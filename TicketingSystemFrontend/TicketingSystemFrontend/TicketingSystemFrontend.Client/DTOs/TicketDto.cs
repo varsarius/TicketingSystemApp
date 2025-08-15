66593c25f4e0972568de3db3187a7541b389171e
Andrey Uzun <uzun.andrei13@gmail.com>
2025-08-15 15:24:45 +0300

    shortened the code in home.razor via splitting into components + changes to the positioning in ticketCreate

--- TicketingSystemFrontend/TicketingSystemFrontend/TicketingSystemFrontend.Client/DTOs/TicketDto.cs
+++ TicketingSystemFrontend/TicketingSystemFrontend/TicketingSystemFrontend.Client/DTOs/TicketDto.cs
@@ -7,9 +7,16 @@ public class TicketDto
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public Priority Priority { get; set; }
     public string CategoryName { get; set; } = string.Empty;
+    public int TicketCategoryId { get; set; }
+    public string AgentName { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public Guid AgentId { get; set; }
+    public Guid UserId { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
+    public Priority Priority { get; set; }
+    public Status Status { get; set; }
+
 }
 
